776789fc840ca4bb30483c96dd81e94c040ec0d0
AlanAytbaev <AlanAytbaev@users.noreply.github.com>
2020-01-17 19:58:48 -0500

    Added gradient, textual changes

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ var homePos = home.offsetTop;
 var aboutPos = document.querySelector('#about').offsetTop;
 var skillsPos = document.querySelector('#skills').offsetTop;
 var projectsPos = document.querySelector('#projects').offsetTop;
-	
+
 function fixedNavigation() {
-	
+
 	var pagePos = window.pageYOffset;
 	var navBottomPos = navPos + navBar.clientHeight;
 	var pageBottom = pagePos + window.clientHeight;
@@ -19,11 +19,11 @@ function fixedNavigation() {
 		navBar.classList.remove('fixed');
 	}
 	//about.getBoundingClientRect().top + window.pageYOffset
-	if(pagePos <= aboutPos) {
+	if (pagePos <= aboutPos) {
 		highlightLink('#nav-home');
-	} else if(pagePos <= skillsPos) {
+	} else if (pagePos <= skillsPos) {
 		highlightLink('#nav-about');
-	} else if(pagePos <= projectsPos) {
+	} else if (pagePos <= projectsPos) {
 		highlightLink('#nav-skills');
 	} else {
 		highlightLink('#nav-projects');
@@ -37,16 +37,16 @@ function fixedNavigation() {
 }
 
 // scroll-triggered animation of elements
-var windowHeightOffset = window.innerHeight*0.7;
+var windowHeightOffset = window.innerHeight * 0.7;
 
 function checkElements() {
 	var elems = document.querySelectorAll('.hidden');
-	elems.forEach(function(item, index) {
-		var windowPos = window.pageYOffset + windowHeightOffset + 100;
-		scrollTop = window.pageYOffset 
+	elems.forEach(function (item, index) {
+		var windowPos = window.pageYOffset + windowHeightOffset + 200;
+		scrollTop = window.pageYOffset
 		var rect = item.getBoundingClientRect();
 		var topPos = rect.top + window.pageYOffset;
-		if(topPos <= windowPos) {
+		if (topPos <= windowPos) {
 			var animationClass = item.getAttribute("animationClass");
 			item.className = item.className.replace('hidden', animationClass);
 		}
@@ -57,13 +57,13 @@ function pageNavigation(event) {
 	console.log("Clicked");
 	var source = event.target;
 	console.log(source);
-	if(source.classList.contains('nav-item')) {
+	if (source.classList.contains('nav-item')) {
 		var destinationString = source.getAttribute("dest");
 		console.log(destinationString);
 		var destinationElement = document.querySelector('#' + destinationString);
 		console.log(destinationElement);
 		var y = destinationElement.offsetTop;
-		window.scrollTo(0, y+1);
+		window.scrollTo(0, y + 1);
 	}
 }
 window.addEventListener('scroll', fixedNavigation);
